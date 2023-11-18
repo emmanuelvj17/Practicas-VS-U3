de48122e0fe701a41538f394639fc3ca61393a6e
emmanuelvj17 <148415063+emmanuelvj17@users.noreply.github.com>
2023-11-17 18:51:52 -0600

    Add files via upload

---
+++ Practica 12/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Practica_12
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            label1 = new Label();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(254, 112);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(156, 23);
+            textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI Black", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(115, 114);
+            label1.Name = "label1";
+            label1.Size = new Size(101, 21);
+            label1.TabIndex = 1;
+            label1.Text = "Calificacion";
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Red;
+            button1.Font = new Font("Segoe UI Black", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.Location = new Point(197, 224);
+            button1.Name = "button1";
+            button1.Size = new Size(138, 49);
+            button1.TabIndex = 2;
+            button1.Text = "Decision";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(537, 399);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Controls.Add(textBox1);
+            Name = "Form1";
+            Text = "Calificacion";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private Label label1;
+        private Button button1;
+    }
+}
---
+++ Practica 12/Form1.cs
@@ -0,0 +1,20 @@
+using System.Runtime.CompilerServices;
+
+namespace Practica_12
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double cal;
+            cal = double.Parse(textBox1.Text);
+            if (cal < 3.0) MessageBox.Show("REPROBADO");
+
+        }
+    }
+}

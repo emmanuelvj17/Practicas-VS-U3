b46ce330c8c10b4c473f4fc220dd3879ef2426e0
emmanuelvj17 <148415063+emmanuelvj17@users.noreply.github.com>
2023-11-17 18:54:52 -0600

    Add files via upload

---
+++ Practica 14/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Practica_14
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            textBox1 = new TextBox();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(131, 102);
+            label1.Name = "label1";
+            label1.Size = new Size(93, 21);
+            label1.TabIndex = 0;
+            label1.Text = "Calificacion";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(259, 104);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(134, 23);
+            textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Red;
+            button1.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.Location = new Point(204, 188);
+            button1.Name = "button1";
+            button1.Size = new Size(112, 47);
+            button1.TabIndex = 2;
+            button1.Text = "Decision";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(128, 255, 255);
+            ClientSize = new Size(534, 344);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox textBox1;
+        private Button button1;
+    }
+}
